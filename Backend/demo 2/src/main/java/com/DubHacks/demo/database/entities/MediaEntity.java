1196395ee8987b5ef617e161962928849f534b48
GitHubAccount-F <the.blue.man13579@gmail.com>
2025-10-18 13:10:59 -0700

    First Commit

---
+++ Backend/demo 2/src/main/java/com/DubHacks/demo/database/entities/MediaEntity.java
@@ -0,0 +1,12 @@
+package com.DubHacks.demo.database.entities;
+
+public class MediaEntity {
+
+    public int id;
+    public String title;
+    public String description;
+    public double rating;
+    public double budget;
+
+
+}
