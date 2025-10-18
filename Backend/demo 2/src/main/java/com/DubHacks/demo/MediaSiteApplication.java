b9dc5e6d26405d8e5975b355bb2d31d456a469ab
GitHubAccount-F <the.blue.man13579@gmail.com>
2025-10-18 13:04:20 -0700

    First Commit

---
+++ Backend/demo 2/src/main/java/com/DubHacks/demo/MediaSiteApplication.java
@@ -0,0 +1,13 @@
+package com.DubHacks.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaSiteApplication.class, args);
+	}
+
+}
